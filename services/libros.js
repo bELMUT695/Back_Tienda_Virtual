d991229aebfca93fad6051fd05746086daeb1d72
bELMUT695 <bELMUT695>
2021-10-15 10:36:31 -0500

    aniadiendo

--- services/libros.js
+++ services/libros.js
@@ -2,9 +2,12 @@ const MongoLib = require('../lib/Mongo_Libros');
 
 class BooksService{
     constructor(){
+        
         this.collection='Libros';
         this.mongoDB= new MongoLib();
     }
+
+
 
     async getMovies(){
         const movies = await this.mongoDB.getAll(this.collection /*, query*/)
