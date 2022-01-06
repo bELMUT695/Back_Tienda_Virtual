a76a4000ff3c8c1fca41a1ade95d4d1f20e0eb9a
bELMUT695 <antunezgonzalesv@gmail.com>
2022-01-05 21:01:09 -0500

    Se añadio navbar

--- routes/clothe.js
+++ routes/clothe.js
@@ -12,44 +12,21 @@ function ClotheApi(app){
     const router = express.Router();
     app.use('/api/clothe',router);
     const clothesServices = new ClothesServices();
-    router.get('/', async function (req, res, next) {
-      
-        cacheResponse(res, FIVE_MINUTES_IN_SECONDS);
-        const { tags } = req.query;
-       
-        try {
-          const Clothes = await clothesServices.geClothes(/*{ tags }*/);
-         
-          // throw new Error("Error getting movies");
-          res.status(200).json({
-            data: Clothes,
-            message: 'movies listed',
-          });
-        } catch (error) {
-          next(error);
-        }
-        
-      });
-      
-      
-      router.get("/:clotheId",async function(req,res,next){
-        cacheResponse(res.FIVE_MINUTES_IN_SECONDS);
-        console.log(req.params)
-        const {clotheId}=req.params;
-        try {
-         
-          const Clothe=await clothesServices.getClothe(clotheId)
+   
 
+      router.get("/gender",async function(req,res,next){
+        cacheResponse(res.FIVE_MINUTES_IN_SECONDS);
+   
+        try {
+          const clothes=await clothesServices.getClohtebyGender();
           res.status(200).json({
-            data:Clothe,
-            message:'Clothe encontrado'
+            data:clothes,
+            message:'categorias encontrada'
           });
         } catch (error) {
           next(error)
         }
-      });
-
-
+      })
 
    
    
