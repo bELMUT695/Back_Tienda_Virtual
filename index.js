8a926bb438f8924d84e1a4e641eab1c1266b42af
bELMUT695 <antunezgonzalesv@gmail.com>
2022-02-04 20:33:18 -0500

    Cors

--- index.js
+++ index.js
@@ -7,9 +7,20 @@ const ClotheApi = require("./routes/clothe.js");
 const ClotheUserApi = require("./routes/clothe_user.js");
 const PersonalInformationApi = require("./routes/personal_information.js");
 
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(
   cors({ origin: "https://front-recomendacion-contenidos.herokuapp.com" })
 );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 app.use(express.json());
 ClotheApi(app);
