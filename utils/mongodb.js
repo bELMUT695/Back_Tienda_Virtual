564f537b65007f1b5dfb09362a54efb8b7a03f00
bELMUT695 <antunezgonzalesv@gmail.com>
2022-07-31 17:37:39 -0500

    Add v3 api

--- utils/mongodb.js
+++ utils/mongodb.js
@@ -1,20 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const dbConnection = async () => {
   try {
+    mongoose.set("useCreateIndex", true);
     await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    
-    console.log('Base de datos online');
+
+    console.log("Base de datos online");
   } catch (error) {
     console.log(error);
-    throw new Error('Error a la hora de iniciar la base de datos')
+    throw new Error("Error a la hora de iniciar la base de datos");
   }
-}
-
+};
 
 module.exports = {
   dbConnection,
-}
+};
